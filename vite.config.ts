096c4c87f8185a2206046e484919c89ae328c1a2
Leonardo Falcoski <leonardo.falcoski@gmail.com>
2022-03-13 01:19:54 -0300

    fix: Consertado valor incorreto da configuração de base do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/alura-7-days-in-react',
+  base: '/alura-7-days-of-react',
   plugins: [react()]
 })
